acf21392f6255687ccbcb87ef69135b1e02eb6c9
NJWonE <93060795+NJWonE@users.noreply.github.com>
2022-10-26 09:29:52 +0900

    깃연동 commit

---
+++ src/test/java/com/noh/yaho/YahoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noh.yaho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
