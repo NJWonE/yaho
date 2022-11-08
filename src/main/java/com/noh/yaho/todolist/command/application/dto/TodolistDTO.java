f7000575e186ea1bd4a5f2e1b120115b0a0cf333
NJWonE <nohjaewon123@daum.net>
2022-11-08 22:41:11 +0900

    #20 feat: todolistDTO 파일 추가

---
+++ src/main/java/com/noh/yaho/todolist/command/application/dto/TodolistDTO.java
@@ -0,0 +1,16 @@
+package com.noh.yaho.todolist.command.application.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TodolistDTO {
+    private int memberNo;
+    private int projectNo;
+    private String title;
+    private String content;
+    private java.util.Date dueDate;
+}
