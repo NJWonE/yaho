355e2d469ffdf4e5896d0f3161375b84dec98d1c
NJWonE <nohjaewon123@daum.net>
2022-11-09 17:12:29 +0900

    #20 feat: tag 엔티티 추가
    todolist 에 포함되는 태그 엔티티 추가

---
+++ src/main/java/com/noh/yaho/todolist/command/domain/model/Tag.java
@@ -0,0 +1,26 @@
+package com.noh.yaho.todolist.command.domain.model;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_TAG")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Tag {
+
+    @Id
+    @Column(name="TAG_NO")
+    private int tagNo;
+
+    @Column(name="TAG_NAME")
+    private String tagName;
+}
