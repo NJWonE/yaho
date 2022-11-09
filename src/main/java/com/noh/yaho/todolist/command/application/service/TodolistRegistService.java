15efc260be855584221df32ac7bb55613aff135d
NJWonE <nohjaewon123@daum.net>
2022-11-09 17:23:14 +0900

    #20 fix: todolist 를 저장할대 tag 정보 조회 저장 기능 추가

--- src/main/java/com/noh/yaho/todolist/command/application/service/TodolistRegistService.java
+++ src/main/java/com/noh/yaho/todolist/command/application/service/TodolistRegistService.java
@@ -2,6 +2,7 @@ package com.noh.yaho.todolist.command.application.service;
 
 import com.noh.yaho.todolist.command.application.dto.TodolistDTO;
 import com.noh.yaho.todolist.command.domain.model.Todolist;
+import com.noh.yaho.todolist.command.domain.repository.TagRepository;
 import com.noh.yaho.todolist.command.domain.repository.TodolistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodolistRegistService {
 
     private final TodolistRepository todolistRepository;
+    private final TagRepository tagRepository;
     @Transactional
     public int registTodolist(TodolistDTO todolistDTO) {
         Todolist newTodolist = new Todolist(todolistDTO.getMemberNo(), todolistDTO.getProjectNo(),
-                todolistDTO.getDueDate(), todolistDTO.getTitle(), todolistDTO.getContent());
+                todolistDTO.getDueDate(), todolistDTO.getTitle(), todolistDTO.getContent(), tagRepository.findById(todolistDTO.getTagNo()).get());
+        System.out.println("todolistDTO = " + newTodolist);
         todolistRepository.save(newTodolist);
+
         return newTodolist.getTodolistNo();
     }
 }
