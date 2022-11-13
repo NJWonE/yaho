283f16261d09ed6b6411475cd3423d2847aae3eb
NJWonE <nohjaewon123@daum.net>
2022-11-13 22:05:41 +0900

    #22 feat: member domain repository 추가

--- src/main/java/com/noh/yaho/member/command/domain/repository/MemberRepository.java
+++ src/main/java/com/noh/yaho/member/command/domain/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.noh.yaho.member.command.domain.repository;
 import com.noh.yaho.member.command.domain.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByMemberId(String memberId);
     boolean existsByMemberId(String MemberId);
 }
