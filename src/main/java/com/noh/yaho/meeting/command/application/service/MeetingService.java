bf66f595c3c8e7243193edec49ab6276f3729a9a
NJWonE <nohjaewon123@daum.net>
2022-11-19 21:09:51 +0900

    #25 feat: 미팅 내용을 시간 순서대로 정렬하는 기능 추가

--- src/main/java/com/noh/yaho/meeting/command/application/service/MeetingService.java
+++ src/main/java/com/noh/yaho/meeting/command/application/service/MeetingService.java
@@ -1,4 +1,26 @@
 package com.noh.yaho.meeting.command.application.service;
 
+import com.noh.yaho.meeting.command.application.dto.ConversationDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MeetingService {
+    public List<ConversationDTO> registMeeting(List<ConversationDTO> conversationDTOList) {
+        conversationDTOList.sort(new Comparator<ConversationDTO>() {
+            @Override
+            public int compare(ConversationDTO o1, ConversationDTO o2) {
+                if(o1.getTime().getTime()-o2.getTime().getTime()>1){
+                    return 1;
+                }else{
+                    return -1;
+                }
+            }
+        });
+        return conversationDTOList;
+    }
 }
