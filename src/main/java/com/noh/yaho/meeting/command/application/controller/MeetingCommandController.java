db45377277e6176597bf0cbd17b0190ef44008e2
NJWonE <nohjaewon123@daum.net>
2022-11-19 21:09:01 +0900

    #25 feat: 미팅 내용을 받는 컨트롤로 추가

--- src/main/java/com/noh/yaho/meeting/command/application/controller/MeetingCommandController.java
+++ src/main/java/com/noh/yaho/meeting/command/application/controller/MeetingCommandController.java
@@ -1,14 +1,26 @@
 package com.noh.yaho.meeting.command.application.controller;
 
+import com.noh.yaho.common.dto.ResponseDTO;
+import com.noh.yaho.meeting.command.application.dto.ConversationDTO;
 import com.noh.yaho.meeting.command.application.service.MeetingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/meeting")
 public class MeetingCommandController {
     private final MeetingService meetingService;
 
+    @PostMapping
+    public ResponseDTO registMeeting(@RequestBody List<ConversationDTO> conversationDTO){
+        return new ResponseDTO(HttpStatus.OK, "미팅정보저장성공", meetingService.registMeeting(conversationDTO));
+    }
 }
