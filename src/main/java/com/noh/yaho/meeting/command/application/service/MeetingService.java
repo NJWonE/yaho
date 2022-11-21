e1f2b17a6ef77bdb5ff4632b19433c2c83c9ab49
NJWonE <nohjaewon123@daum.net>
2022-11-21 21:56:56 +0900

    #25 feat: Meeting Entity create 기능 추가, 디자인패턴 변경

--- src/main/java/com/noh/yaho/meeting/command/application/service/MeetingService.java
+++ src/main/java/com/noh/yaho/meeting/command/application/service/MeetingService.java
@@ -3,27 +3,28 @@ package com.noh.yaho.meeting.command.application.service;
 import com.noh.yaho.meeting.command.application.dto.ConversationDTO;
 import com.noh.yaho.meeting.command.application.dto.MeetingDTO;
 import com.noh.yaho.meeting.command.application.dto.MeetingResultDTO;
+import com.noh.yaho.meeting.command.domain.model.Meeting;
+import com.noh.yaho.meeting.command.domain.repository.MeetingRepository;
+import com.noh.yaho.meeting.command.domain.service.AiConnectionService;
+import com.noh.yaho.meeting.command.domain.service.AwsSimpleStorageService;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.http.HttpClient;
+import javax.transaction.Transactional;
 import java.util.Comparator;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class MeetingService {
-    public List<ConversationDTO> registMeeting(MeetingDTO meetingDTO) {
+@Transactional
+public class    MeetingService {
+
+    private final AiConnectionService aiConnectionService;
+    private final AwsSimpleStorageService awsSimpleStorageService;
+    private final MeetingRepository meetingRepository;
+
+    public int registMeeting(MeetingDTO meetingDTO) {
         List<ConversationDTO> conversationDTOList = meetingDTO.getConversation();
         conversationDTOList.sort(new Comparator<ConversationDTO>() {
             @Override
@@ -35,26 +36,21 @@ public class MeetingService {
                 }
             }
         });
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         MultiValueMap<String, List<ConversationDTO>> body = new LinkedMultiValueMap<>();
         body.add("conversation", conversationDTOList);
-        HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
 
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
+        String aiURL = "http://34.64.197.102:9090/summarize";
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setMaxConnTotal(100)
-                .setMaxConnPerRoute(5)
-                .build();
-        factory.setHttpClient(httpClient);
+        MeetingResultDTO meetingResultDTO = aiConnectionService.request(body, aiURL);
+        String imageURL = awsSimpleStorageService.upload(meetingResultDTO.getGraph());
+        Meeting newMeeting = new Meeting(meetingDTO.getProjectNo(), imageURL,
+                meetingResultDTO.getSummary(),
+                meetingResultDTO.getRecord(),
+                meetingResultDTO.getKeyword(),
+                conversationDTOList.get(0).getTime(),
+                conversationDTOList.get(conversationDTOList.size()-1).getTime());
+        meetingRepository.save(newMeeting);
 
-        RestTemplate restTemplate = new RestTemplate(factory);
-        MeetingResultDTO response = restTemplate.postForObject("asdasdasd", requestMessage, MeetingResultDTO.class);
-
-        return conversationDTOList;
+        return newMeeting.getMeetingNo();
     }
 }
