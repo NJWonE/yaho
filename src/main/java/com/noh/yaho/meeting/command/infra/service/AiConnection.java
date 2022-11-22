03bf7fd46840cf2c5ea98c374cf00b2db60a34c9
NJWonE <nohjaewon123@daum.net>
2022-11-22 15:41:56 +0900

    #25 fix: AI 요청 URL 변경

--- src/main/java/com/noh/yaho/meeting/command/infra/service/AiConnection.java
+++ src/main/java/com/noh/yaho/meeting/command/infra/service/AiConnection.java
@@ -10,10 +10,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.List;
 
 @Service
