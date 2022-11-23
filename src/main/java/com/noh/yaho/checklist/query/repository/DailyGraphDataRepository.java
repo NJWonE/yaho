90de2e5a02664e441d0b75acbea1a84f1fa51fc3
NJWonE <nohjaewon123@daum.net>
2022-11-23 16:05:17 +0900

    #27 feat: dailyGraph 조회용 repository 추가

---
+++ src/main/java/com/noh/yaho/checklist/query/repository/DailyGraphDataRepository.java
@@ -0,0 +1,11 @@
+package com.noh.yaho.checklist.query.repository;
+
+import com.noh.yaho.checklist.query.data.DailyGraphData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.Optional;
+
+public interface DailyGraphDataRepository extends JpaRepository<DailyGraphData, Integer> {
+    public Optional<DailyGraphData> findByMemberNoAndCreateDateLike(int MemberNo, Date createDate);
+}
