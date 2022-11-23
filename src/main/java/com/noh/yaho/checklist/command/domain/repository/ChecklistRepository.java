680cce36b3c66d2255f935612f49f153235ac65c
NJWonE <nohjaewon123@daum.net>
2022-11-23 16:04:22 +0900

    #27 feat: checkList 기간 별 조회 기능 추가

--- src/main/java/com/noh/yaho/checklist/command/domain/repository/ChecklistRepository.java
+++ src/main/java/com/noh/yaho/checklist/command/domain/repository/ChecklistRepository.java
@@ -2,6 +2,11 @@ package com.noh.yaho.checklist.command.domain.repository;
 
 import com.noh.yaho.checklist.command.domain.model.Checklist;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ChecklistRepository extends JpaRepository<Checklist, Integer> {
+    public List<Checklist> findByMemberNoAndCreateDateBetween(int memberNo, Date startDate, Date endDate);
 }
 
